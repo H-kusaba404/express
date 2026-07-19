1be03d80b357b0603e89f649c444a4ee74bfba94
H-kusaba404 <kusabaharuna@gmail.com>
2026-07-19 16:28:10 +0900

    第14回編集

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var createError = require('http-errors');
+var createError = require('http-errors'); //⭐︎無くなっていたけど残しとく
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var bodyParser = require('body-parser');  //追記
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
-var artRouter = require('./routes/art');
+var artRouter = require('./routes/art'); //レポート課題で作ったAPI部分
+var notes_from_bRouter = require('./routes/notes_from_b'); //追記（第14回）
 
 var app = express();
 
@@ -17,7 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,7 +30,8 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat',catRouter);
-app.use('/art',artRouter);
+app.use('/art',artRouter); //レポート課題で作ったAPI部分
+app.use('/notes_from_b', notes_from_bRouter); //追記（第14回）
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
